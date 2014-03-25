acb4136c787b9395279b78cf930002ac86cc4288
broonzy <ger_daly@yahoo.com>
2014-03-25 09:30:16 +0000

    added converter

---
+++ test/js/converter.js
@@ -0,0 +1,27 @@
+var Converter = (function () {
+    // private stuff goes here
+
+    var converterValue = 2.2;
+
+    return {
+
+        convertFromImperialToMetric: function (weight) {
+            var num = parseInt(weight);
+            if (isNaN(num) == true){
+                throw new Error ("Not a number");
+            }
+            num = num / converterValue;
+            //	document.getElementById("demo").innerHTML = num;
+            return Math.round(num);
+        },
+        convertFromMetricToImperial: function (weight) {
+            return weight / converterValue
+        }
+    }
+})();
+
+var convert = function () {
+    var weight = document.getElementById("imperial").value;
+    var result = Converter.convertFromImperialToMetric(weight);
+    document.getElementById("metric").innerHTML = result;
+}
---
+++ test/spec/ConverterSpec.js
@@ -0,0 +1,11 @@
+describe ("My Converter", function() {
+    it("should convert imperial weight to metric", function() {
+        expect (Converter.convertFromImperialToMetric(2.2)).toBe(1);
+    })
+    it("should convert imperial weight to metric", function() {
+        expect (Converter.convertFromImperialToMetric(2.6)).toBe(1);
+    })
+    it("should be able to deal with strings", function() {
+        expect (function() {Converter.convertFromImperialToMetric("hello")}).toThrow(new Error("Not a number"));
+    })
+})
